a705adc06b177dbc95704a7fa8d32a23fb274f65
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2025-12-04 17:30:13 +0530

    Fix: Hardcode Supabase credentials in next.config.js to resolve build issue

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: 'http://supabasekong-m8s8s44s0w8s0kockwwgckg4.72.60.202.93.sslip.io',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc2NDc4NTM0MCwiZXhwIjo0OTIwNDU4OTQwLCJyb2xlIjoiYW5vbiJ9.rtwITimbW3H1pAlGHaosRMYSVB0vDeNB2irOTCyDBRU',
   },
   trailingSlash: true,
 }
